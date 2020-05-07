967b3c74fdd6d64a5478cf78eaf226c29a519589
Matrixgek91 <jschouw1@gmail.com>
2020-05-07 16:37:34 +0200

    Build 0004 - Some heroku config.........

--- src/main/java/com/example/coronaApp/CoronaAppApplication.java
+++ src/main/java/com/example/coronaApp/CoronaAppApplication.java
@@ -2,9 +2,15 @@ package com.example.coronaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CoronaAppApplication {
+public class CoronaAppApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoronaAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaAppApplication.class, args);
